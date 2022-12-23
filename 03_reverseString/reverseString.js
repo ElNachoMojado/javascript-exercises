a7861773d14b70dccb6a7bba6981c4e3103725d7
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2022-12-23 16:13:11 +0000

    Solve tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let num=string.length;
+    let letter;
+    let revword;
+    if (string === '') {
+        return string;
+    } else {
+        for (i = num; i > 0; i--) {
+            letter=string.slice(i-1, i);
+            (i === num) ? revword = letter
+            :revword += letter;
+        }
+        return revword;
+    }
 };
 
 // Do not edit below this line
