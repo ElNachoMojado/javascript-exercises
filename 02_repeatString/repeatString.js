d455b43fd8cce34c8110de9e8435631e22ea89aa
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2022-12-23 15:40:18 +0000

    Solve tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num === 0 || string != 'hey') {
+        string = '';
+        return string;
+    } else if (num < 0) {
+        string = 'ERROR';
+        return string;
+    } else {
+        for (let i = 0; i < (num-1); i++) {
+        string += 'hey';
+        }
+        return string;
+    }
 };
 
 // Do not edit below this line
