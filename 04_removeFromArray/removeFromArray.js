dff97d17d72d68e7e54ed7d28e9dcdbebdf51c7d
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2022-12-29 19:27:38 +0000

    Solve exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...remove) {
+    const newArray = [];
+    let equal = 0;
+    for (i = 0; i < array.length; i++) {
+        for (j = 0; j < remove.length; j++) {
+            if (array[i] === remove[j]) {
+                equal = 1;
+            }
+        }
+        if (equal === 0) {
+            newArray.push(array[i]);
+        }
+        equal = 0;
+    }
+   
+    return newArray;
 };
 
 // Do not edit below this line
