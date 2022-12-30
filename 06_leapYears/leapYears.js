4a40fb8d7e74e6b981ff5e0e1baae60ccdc59f81
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2022-12-30 16:08:19 +0000

    Solve exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let answer;
+    if (year%4 === 0 && year%100 !== 0) {
+        answer = true;
+        return answer;
+    } else if (year%400 === 0) {
+        answer = true;
+        return answer;
+    } else {
+        answer = false;
+        return answer;
+    }
 };
 
 // Do not edit below this line
