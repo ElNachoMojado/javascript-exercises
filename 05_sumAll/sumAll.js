3fb246cdeaca133348726764c75952e0b5658c9f
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2022-12-30 15:07:31 +0000

    Solve exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    let altStart;
+    let altEnd;
+    if (start < 0 || end < 0 || typeof start !== 'number' || typeof end !== 'number') {
+        sum = 'ERROR';
+    } else {
+        if (start < end) {
+            for (start; start <= end; start++) {
+                sum += start;
+            }
+        } else {
+            altStart = end;
+            altEnd = start;
+            for (altStart; altStart <= altEnd; altStart++) {
+                sum += altStart;
+            }
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
